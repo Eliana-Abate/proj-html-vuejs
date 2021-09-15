88dcd718e41b486f5ee9133e554ae9edfe336712
Eliana-Abate <eliana.abate@outlook.it>
2021-09-15 11:18:17 +0200

    added basic components

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <h1>Test</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>Test</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
