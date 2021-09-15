682e1f6a73a7b10327489501dec121ddd463bc34
Eliana-Abate <eliana.abate@outlook.it>
2021-09-15 13:18:31 +0200

    imported Fontawesome + Google Font Roboto; completed section top-header in Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,26 @@
 <template>
   <header>
+    <!-- TOP HEADER -->
     <section id="top-header">
-      <div class="container">
-        <div class="row">
-          <p>Call us for a Free Quote: 1.800.555.6789</p>
-          <p></p>
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <!-- Contact -->
+          <div class="col-6">
+            <p class="m-0">Call us for a Free Quote: 1.800.555.6789</p>
+          </div>
+
+          <!-- Social Icons -->
+          <div class="col-6 text-end">
+            <span class="mx-3"><i class="fab fa-facebook-f"></i></span>
+            <span class="mx-3"><i class="fab fa-twitter"></i></span>
+            <span class="mx-3"><i class="fab fa-instagram"></i></span>
+            <span class="ms-3"><i class="fab fa-youtube"></i></span>
+          </div>
         </div>
       </div>
     </section>
+
+    <!-- BOTTOM HEADER -->
     <section id="bottom-header"></section>
   </header>
 </template>
@@ -26,7 +39,11 @@ header {
     height: 50px;
     background-color: $lima-dark-vibrant;
     color: white;
+    span {
+      font-size: 1.1em;
+    }
   }
+
   #bottom-header {
     height: 110px;
     background-color: violet;
