36e137c5425800633d1c74021bbdc3107f0d9876
Eliana-Abate <eliana.abate@outlook.it>
2021-09-17 15:03:03 +0200

    completed section #utilities in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,9 @@
       <div class="container h-100">
         <div class="row h-100 align-items-center">
           <div class="col-4">
-            <h4 class="fw-700">ABOUT</h4>
-            <p>
+            <div></div>
+            <h4 class="fw-700 mb-5">ABOUT</h4>
+            <p class="mb-5">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
               convallis ex ut sem accumsan vehicula. Integer elit arcu,
               hendrerit id sapien ut, facilisis accumsan arcu.
@@ -14,6 +15,26 @@
               src="../assets/images/avada-movers-logo.png"
               alt="avada-movers-logo"
             />
+          </div>
+
+          <div class="col-4">
+            <h4 class="fw-700 mb-5">AVADA MOVERS</h4>
+            <ul v-for="(item, id) in footerNavbar" :key="id" class="p-0">
+              <li>
+                <i class="fas fa-angle-right pe-2"></i>
+                {{ item.link }}
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-4">
+            <h4 class="fw-700 mb-5">RECENT POSTS</h4>
+            <ul v-for="(item, id) in postsLink" :key="id" class="p-0">
+              <li>
+                <i class="fas fa-angle-right pe-2"></i>
+                {{ item.link }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -26,6 +47,25 @@
 <script>
 export default {
   name: "Footer",
+  data() {
+    return {
+      footerNavbar: [
+        { link: "Home", id: "1" },
+        { link: "Rates", id: "2" },
+        { link: "Testimonials", id: "3" },
+        { link: "Blog", id: "4" },
+        { link: "Free Quote", id: "5" },
+      ],
+
+      postsLink: [
+        { link: "Heading Out To College?", id: "1" },
+        { link: "Moving Your Business?", id: "2" },
+        { link: "Outstanding Quality", id: "3" },
+        { link: "Cost of Moving", id: "4" },
+        { link: "Best Moving Tips", id: "5" },
+      ],
+    };
+  },
 };
 </script>
 
@@ -37,6 +77,14 @@ footer {
 
 #utilities {
   height: 450px;
+  ul {
+    li {
+      list-style-type: none;
+    }
+    li:hover {
+      color: $lima;
+    }
+  }
 }
 
 #legal-info {
